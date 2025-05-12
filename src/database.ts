8fe27ed888852a54077ba96682a842dfaedcc561
Pedro Dias <pdr050203@gmail.com>
2025-05-12 08:29:30 -0300

    feat: add base migrations config

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,12 @@ import { knex as setupKnex, Knex } from "knex";
 export const config: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: "./tmp/app.db",
+    filename: "./db/app.db",
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
   },
 };
 
