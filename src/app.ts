cc16bb6d6bba89ade21c37a927ff647277374a5a
Pedro Dias <pdr050203@gmail.com>
2025-06-23 01:19:46 -0300

    feat: add cookies and meals creation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
 import { mealsRoutes } from "./routes/meals";
+import { usersRoutes } from "./routes/users";
 
 export const app = fastify();
 
-app.register(mealsRoutes, {
-  prefix: "meals",
-});
+app.register(cookie);
+
+app.register(usersRoutes, { prefix: "users" });
+
+app.register(mealsRoutes, { prefix: "meals" });
